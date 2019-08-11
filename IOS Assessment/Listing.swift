3d4eda494696d5fa098e9c3dff2756cc483f92d5
justinedwards <justinedwards1701@yahoo.com>
2019-08-11 11:55:37 -0400

    main

---
+++ IOS Assessment/Listing.swift
@@ -0,0 +1,79 @@
+//
+//  Listing.swift
+//  IOS Assessment
+//
+//  Created by Justin Edwards on 8/7/19.
+//  Copyright © 2019 apps. All rights reserved.
+//
+
+import UIKit
+
+
+
+class Listings: Codable {
+    var listings: [Listing]
+    
+    init(listings: [Listing]) {
+       self.listings = listings
+    }
+}
+
+class Listing: Codable {
+  //  var photo: UIImage!
+    var images: Images!
+    var year: Int!
+     var make: String!
+     var model: String!
+     var trim: String!
+    var currentPrice: Int!
+    var mileage: Int!
+    var location: String!
+    var dealer: dealer!
+    
+    
+    
+    init( images: Images!,year: Int!, make: String!, model:String!, trim:String!, currentPrice:Int!, mileage:Int!, location : String!, dealer: dealer!){
+        
+      //  self.photo = photo
+        self.images = images
+        self.year = year
+        self.make = make
+        self.model = model
+        self.trim = trim
+        self.currentPrice = currentPrice
+        self.mileage = mileage
+        self.location = location
+        self.dealer = dealer
+       
+        
+    }
+    
+}
+
+class Images: Codable {
+    var firstPhoto: FirstPhoto!
+    init( firstPhoto: FirstPhoto!){
+        self.firstPhoto = firstPhoto
+    }}
+
+class FirstPhoto: Codable {
+    //  var photo: UIImage!
+    var small: String!
+    
+    init( small: String!){
+        //  self.photo = photo
+        self.small = small
+    }}
+
+
+class dealer: Codable {
+    var city: String!
+    var state: String!
+    var phone: String!
+    init( city: String!, state: String!, phone:String!){
+        self.city = city
+        self.state = state
+        self.phone = phone
+    }}
+
+
---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'IOS Assessment' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for IOS Assessment
+
+  target 'IOS AssessmentTests' do
+   pod 'PhoneNumberKit', '~> 3.0'
+  end
+
+  target 'IOS AssessmentUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
